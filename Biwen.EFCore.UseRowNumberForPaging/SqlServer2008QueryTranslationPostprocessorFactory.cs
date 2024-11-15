3593757226e58e07eac848c4207efe2b64d66680
万雅虎 <vipwan@outlook.com>
2024-11-15 14:44:18 +0800

    typos

--- Biwen.EFCore.UseRowNumberForPaging/SqlServer2008QueryTranslationPostprocessorFactory.cs
+++ Biwen.EFCore.UseRowNumberForPaging/SqlServer2008QueryTranslationPostprocessorFactory.cs
@@ -1,6 +1,9 @@
 ﻿// Licensed to the Biwen.EFCore.UseRowNumberForPaging under one or more agreements.
-// The Biwen.EFCore.UseRowNumberForPaging licenses this file to you under the MIT license.
+// The Biwen.EFCore.UseRowNumberForPaging licenses this file to you under the MIT license. 
 // See the LICENSE file in the project root for more information.
+// Biwen.EFCore.UseRowNumberForPaging Author: 万雅虎, Github: https://github.com/vipwan
+// Bring back support for UseRowNumberForPaging in EntityFrameworkCore 9.0/8.0/7.0/6.0 Use a ROW_NUMBER() in queries instead of OFFSET/FETCH. This method is backwards-compatible to SQL Server 2005.
+// Modify Date: 2024-11-15 14:42:01 SqlServer2008QueryTranslationPostprocessorFactory.cs
 
 namespace Biwen.EFCore.UseRowNumberForPaging;
 
---
+++ Biwen.EFCore.UseRowNumberForPaging/_GlobalUsings.cs
@@ -0,0 +1,13 @@
+﻿// Licensed to the Biwen.EFCore.UseRowNumberForPaging under one or more agreements.
+// The Biwen.EFCore.UseRowNumberForPaging licenses this file to you under the MIT license. 
+// See the LICENSE file in the project root for more information.
+// Biwen.EFCore.UseRowNumberForPaging Author: 万雅虎, Github: https://github.com/vipwan
+// Bring back support for UseRowNumberForPaging in EntityFrameworkCore 9.0/8.0/7.0/6.0 Use a ROW_NUMBER() in queries instead of OFFSET/FETCH. This method is backwards-compatible to SQL Server 2005.
+// Modify Date: 2024-11-15 14:41:49 _GlobalUsings.cs
+
+global using System;
+global using System.Linq;
+global using System.Linq.Expressions;
+global using Microsoft.EntityFrameworkCore.Infrastructure;
+global using Microsoft.EntityFrameworkCore.Query;
+global using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
